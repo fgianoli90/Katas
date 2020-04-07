c19fb7b1cc56bc23f3eda6257b6930a10517705c
fgianoli90 <fgianoli90@gmail.com>
2020-04-07 00:30:30 -0400

    reduced to one line using methods filter and map

--- 6kyu/ReplaceAlphaPosition.js
+++ 6kyu/ReplaceAlphaPosition.js
@@ -1,9 +1,12 @@
 function alphabetPosition(text) {
-var result = "";
-  for (var i = 0; i < text.length; i++) {
-    var code = text.toUpperCase().charCodeAt(i)
-    if (code > 64 && code < 91) result += (code - 64) + " ";
-  }
+    return text.toUpperCase().split('')     //Make letters all uppercase, split into array 
 
-  return result.slice(0, result.length - 1);
+                .filter(eachLet=>{          //Filter for letters using charCodeAt() (64 < alphabet < 91) 
+                    return eachLet.charCodeAt() > 64
+                }).filter(eachLet=>{
+                    return eachLet.charCodeAt() <91
+                }).map(eachLet=>{           //map the filtered array returning the position of letter in aplhabet
+                    return eachLet.charCodeAt()-64
+                }).join(" ")                //join array to return string
+
 }
