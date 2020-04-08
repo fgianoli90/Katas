cc2d2e513baff59cfd718a83de5f8f11da41c4c0
fgianoli90 <fgianoli90@gmail.com>
2020-04-08 14:45:58 -0400

    responsibly drinking optimized

--- 7kyu/ResponsibleDrinking.js
+++ 7kyu/ResponsibleDrinking.js
@@ -1,13 +1,9 @@
 function hydrate(s) {
-    let x=s.split(' ').filter(each=>{return Number(each)})
-    if (x.length <2){
-      x=x.join('')
-        if(x==1){
-            return x + " glass of water"
-        }else{
-            return x + " glasses of water"
-        }
-    }else if( x.length >1){
-        return x.map(eachNum=>{return Number(eachNum)}).reduce((accum,val)=>{return accum+val},0)+" glasses of water"
+    let x=s.split(' ').filter(each=>{return Number(each)}).map(eachNum=>{return Number(eachNum)}).reduce((accum,val)=>{return accum+val},0)
+
+    if(x==1){
+        return x + " glass of water"
+    }else{
+       return x + " glasses of water"
     }
-   }
+}
